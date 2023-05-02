6abf15f59308919d0804d82981eea49fc6b416c5
Flor_Fernández <florencia.fernandez.051@gmail.com>
2023-05-02 14:49:46 -0300

    BFS algorithm

--- src/pathfinder/search/bfs.py
+++ src/pathfinder/search/bfs.py
@@ -21,7 +21,40 @@ class BreadthFirstSearch:
         # Initialize the explored dictionary to be empty
         explored = {} 
         
-        # Add the node to the explored dictionary
+        # Mark the node as explored
         explored[node.state] = True
+
+        # Check if first node (root) is solution
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
         
-        return NoSolution(explored)
+        # Initialize the frontier with the initial node
+        frontier = QueueFrontier()
+        frontier.add(node)
+
+        while True:
+
+            #  Fail if the frontier is empty
+            if frontier.is_empty():
+                return NoSolution(explored)
+
+            # Remove a node from the frontier
+            node = frontier.remove()
+
+            neighbours = grid.get_neighbours(node.state)
+            for possible_action in neighbours:
+                new_state = neighbours[possible_action]
+                child_node = Node("", new_state , node.cost + grid.get_cost(new_state))
+                child_node.parent = node
+                child_node.action = possible_action
+
+                # Return if the node contains a goal state
+                if child_node.state == grid.end:
+                    return Solution(node, explored)
+                
+                if child_node.state not in explored:
+                    # Add the node to the explored dictionary
+                    explored[child_node.state] = True
+                    # Add the new node to the frontier
+                    frontier.add(child_node)
