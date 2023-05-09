ba15de0febbd81c1d3d47af37ec793b0d27b631f
salvanya <leandrosalvana@gmail.com>
2023-05-09 11:18:46 -0300

    Astar algorithm added

--- src/pathfinder/search/gbfs.py
+++ src/pathfinder/search/gbfs.py
@@ -2,12 +2,18 @@ from ..models.grid import Grid
 from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
+from math import sqrt
 
 
+def heuristic(node_state, end):
+            x1, y1 = node_state
+            x2, y2 = end
+            return int(sqrt((x2 - x1)**2 + (y2 - y1)**2))
+            
 class GreedyBestFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
-        """Find path between two points in a grid using Greedy Best First Search
+        """Find path between two points in a grid using A* Search
 
         Args:
             grid (Grid): Grid of points
@@ -16,12 +22,47 @@ class GreedyBestFirstSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node = Node("", grid.start, heuristic(grid.start, grid.end))
+
+        # Initialize the frointier
+        frontier = PriorityQueueFrontier()
+
+        # Add the root to the frontier
+        frontier.add(node, node.cost)
 
         # Initialize the explored dictionary to be empty
         explored = {} 
         
         # Add the node to the explored dictionary
-        explored[node.state] = True
+        explored[node.state] = node
         
-        return NoSolution(explored)
+        while True:
+            # Fail if the froiner is empty
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            # Pop a node from the frontier
+            node = frontier.pop()
+
+            # If the node is the goal, return the solution
+            if node.state == grid.end:
+                return Solution(node, explored)
+
+            # Add neightbours to frontier 
+            neightbours = grid.get_neighbours(node.state)
+            
+            for neightbour in neightbours:
+                new_state = neightbours[neightbour]
+                new_cost = heuristic(node.state, grid.end)
+
+                if new_state not in explored or new_cost < explored[new_state].cost:
+                    new_node = Node("", new_state, new_cost)
+                    new_node.parent = node
+                    new_node.action = neightbour
+
+                    # Add the node to explored
+                    explored[new_node.state] = new_node
+
+                    #Add the node to frontier
+                    frontier.add(new_node, new_node.cost)
+
